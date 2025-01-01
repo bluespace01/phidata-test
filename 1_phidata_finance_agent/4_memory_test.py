351d6b3e2978b082e9e2870b070d7446d70a4c7c
bluespace01 <frank.sha01@qq.com>
2025-01-01 20:06:50 +0800

    Update user memory test case.

--- 1_phidata_finance_agent/4_memory_test.py
+++ 1_phidata_finance_agent/4_memory_test.py
@@ -1,17 +1,60 @@
 from phi.agent import Agent
+from phi.tools.yfinance import YFinanceTools
+from phi.storage.agent.sqlite import SqlAgentStorage
+
+from phi.agent import AgentMemory
+from phi.memory.db.sqlite import SqliteMemoryDb
+
+from phi.model.ollama import Ollama
 from phi.model.groq import Groq
 
+
 agent = Agent(
-    model=Groq(id="llama-3.1-8b-instant"),
-    markdown=True, 
-    debug_mode=True,
-    monitoring=True
-    )
+  #model=Ollama(id="llama3.2"),
+  #model=Groq(id="llama-3.1-8b-instant"),
+  model=Groq(id="llama-3.3-70b-versatile"),
 
-agent.print_response("Share me the way to learn python")
+  tools=[YFinanceTools(
+    stock_price=True, 
+    analyst_recommendations=True, 
+    stock_fundamentals=True
+  )],
+
+  instructions="Use tables to display data.",
+  show_tool_calls=True, 
+  markdown=True,
+  add_history_to_messages=True,
+  debug_mode=True,
+
+  storage=SqlAgentStorage(
+    table_name="agent_sessions", db_file="tmp/agent_storage.db"
+  ),
+  memory=AgentMemory(
+    db=SqliteMemoryDb(
+      table_name="agent_memory", 
+      db_file="tmp/agent_memory.db",
+    ),
+    create_user_memories=True, 
+    update_user_memories_after_run=True,
+  ),
+  user_id="john_billings",
+)
+
+agent.print_response(
+  message="""
+  My name is Mark and I have the following holdings:
+  10,000 of SNOW
+  2,000 of MDB
+  4,500 of DDOG
+  """,
+  stream=True
+)
 
 
-
-
-
+agent.print_response(
+  message="""
+  What's the current stock value of my portfolio?
+  """,
+  stream=True
+)
 
